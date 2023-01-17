18238a1733740dd539b58a9abe68039e12c1cac0
Robin-St <robin@farsk.net>
2023-01-17 21:34:22 +0100

    Update project layout

--- collect_all_partial_wakeword_cli.py
+++
@@ -1,7 +0,0 @@
-from functions import collect_all_partial_wake_word_recording, collect_background_noise, collect_background_noise
-
-
-def collect_all_partial_wakeword_cli(slug, wakeword_split):
-    collect_background_noise(slug, nr=11)
-    collect_all_partial_wake_word_recording(slug, wakeword_split)
-    collect_background_noise(slug, nr=12)
---
+++ src/wakeword_collector/collect_partial_wakeword_cli.py
@@ -0,0 +1,8 @@
+from wakeword_collector.functions import collect_partial_wakeword_recording
+
+def collect_partial_wakeword_cli(slug, wakeword):
+    print(f'Collect partial wake word(s) contained in {wakeword} that should not wake by themselves\n')
+    print('Example:\n')
+    print('For "hey Jarvis" both "hey" AND "Jarvis" should be required for a sucessful wake.\n')
+    print('Therefore we need to record just "Jarvis" in this case ("hey" is already recorded in the syllable collection).\n')
+    collect_partial_wakeword_recording(slug, i=0)
